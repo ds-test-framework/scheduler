0b9747f2a959706cb893cd03b1f6ebd232d05188
Srinidhi N <srinidhin@outlook.com>
2021-04-21 13:08:29 +0530

    temp bug fix tendermintworkload

--- pkg/algos/tendermint/workload.go
+++ pkg/algos/tendermint/workload.go
@@ -1,10 +1,7 @@
 package tendermint
 
 import (
-	"net/http"
-
 	"github.com/ds-test-framework/scheduler/pkg/algos/common"
-	transport "github.com/ds-test-framework/scheduler/pkg/transports/http"
 	"github.com/ds-test-framework/scheduler/pkg/types"
 )
 
@@ -21,11 +18,5 @@ func (w *WorkloadInjector) SetPeerStore(p *common.PeerStore) {
 }
 
 func (w *WorkloadInjector) InjectWorkLoad() *types.Error {
-	var peer *common.Replica
-	for _, p := range w.peers.Iter() {
-		peer = p
-		break
-	}
-	_, err := transport.SendMsg(http.MethodGet, peer.Addr+`/broadcast_tx_commit?tx="name=srinidhi"`, "")
-	return err
+	return nil
 }

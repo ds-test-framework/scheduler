e130d397946c93b878022cb735a8e28bfec4d58b
Srinidhi N <srinidhin@outlook.com>
2021-08-14 10:00:57 +0530

    fixing main

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/ds-test-framework/scheduler/cmd/checker"
-)
-
 func main() {
-	cmd := checker.CheckerCommand()
-	if err := cmd.Execute(); err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
 }
